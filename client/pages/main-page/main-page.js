86e59a8954c9bf5749379c2a4f8667d86423c141
PoBlue <250299430@qq.com>
2018-08-17 21:12:24 +0800

    [logic] page navigation

--- client/pages/main-page/main-page.js
+++ client/pages/main-page/main-page.js
@@ -28,52 +28,27 @@ Page({
   
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  posterClick: function(e) {
+    wx.navigateTo({
+      url: '../movie-detail/movie-detail'
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  hotBtnClick: function(e) {
+    wx.navigateTo({
+      url: '../movie-hot-list/movie-hot-list'
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  mineBtnClick: function(e) {
+    wx.navigateTo({
+      url: '../review-mine/review-mine'
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  personClick: function(e) {
+    wx.navigateTo({
+      url: '../review-detail/review-detail'
+    })
   }
 })
