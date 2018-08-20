bf80582f57e33761125beed345137a71c42a45e5
PoBlue <250299430@qq.com>
2018-08-20 21:18:16 +0800

    [feature] 登陆授权

--- client/pages/review-mine/review-mine.js
+++ client/pages/review-mine/review-mine.js
@@ -1,4 +1,7 @@
 // pages/review-mine/review-mine.js
+const qcloud = require('../../vendor/wafer2-client-sdk/index')
+const config = require('../../config.js')
+
 Page({
 
   /**
@@ -43,7 +46,81 @@ Page({
           voice: "15s"
         }
       }
-    ]
+    ],
+    userInfo: null
+  },
+
+  onLoad: function(options) {
+    this.checkSession({
+      success: ({userInfo})=> {
+        this.setData({
+          userInfo: userInfo
+        })
+      },
+      error: () => {}
+    })
+  },
+
+  checkSession({ success, error }) {
+    wx.checkSession({
+      success: () => {
+        this.getUserInfo({ success, error })
+      },
+      fail: () => {
+        error && error()
+      }
+    })
+  },
+
+  onTapLogin: function(e) {
+    qcloud.setLoginUrl(config.service.loginUrl)
+
+    this.doQcloudLogin({
+      success: ({userInfo}) => {
+        this.setData({
+          userInfo
+        })
+      }
+    })
+  },
+
+  doQcloudLogin({ success, error }) {
+    qcloud.login({
+      success: result => {
+        if (result) {
+          let userInfo= result
+          success && success({
+            userInfo
+          })
+        }
+      },
+      fail: result => {
+        this.getUserInfo({success, error})
+      }
+    })
+  },
+
+  getUserInfo({ success, error }) {
+    qcloud.request({
+      url: config.service.requestUrl,
+      login:true,
+      success: result => {
+        let data = result.data
+
+        if (!data.code) {
+          let userInfo= data.data
+
+          success && success({
+            userInfo
+          })
+        } else {
+          error && error()
+        }
+      }, 
+      fail: () => {
+        error && error()
+      }
+    })
   },
 
   backHomeClick: function(e) {
